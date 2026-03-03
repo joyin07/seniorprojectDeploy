b982f9a04e4881f5a8662c722a973b8309f563c9
Joyin Ang <joyinang2022@gmail.com>
2026-03-03 00:18:15 -0500

    feat: tokens input and add courses

--- assessly/src/App.tsx
+++ assessly/src/App.tsx
@@ -5,6 +5,8 @@ import './styles/App.css'
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard'
 import Login from './pages/login';
+import AddCourses from './pages/addCourses';
+import Tokens from './pages/tokens';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} /> 
+        <Route path="/tokens" element={<Tokens />} /> 
+        <Route path="/add-courses" element={<AddCourses />} /> 
       </Routes>
     </HashRouter>
   )
